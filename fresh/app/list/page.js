269de239bf62faab0856a2f67fef06d061f7060e
ktyoung <ktyoung0507@gmail.com>
2023-05-08 21:40:13 +0900

    수량 증감 버튼(array, object 자료형 state 변경)

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -4,9 +4,12 @@ import Image from "next/image";
 import food0 from "/public/food0.png";
 import { useState } from "react";
 
+// 1. array, object 자료형 state 변경
+// 1-1. ... 연산자로 복사해서 수정
+
 export default function List() {
   let product = ["Tomatoes", "Pasta", "Coconut"];
-  let [count, setCount] = useState(0);
+  let [count, setCount] = useState([0, 0, 0]);
 
   return (
     <div>
@@ -18,15 +21,19 @@ export default function List() {
             <h4>{a} $40</h4>
             <button
               onClick={() => {
-                setCount(count - 1);
+                let copy = [...count]; // 독립적인 array로 복사 → deep copy
+                copy[i]--;
+                setCount(copy);
               }}
             >
               -
             </button>
-            <span>{count}</span>
+            <span>{count[i]}</span>
             <button
               onClick={() => {
-                setCount(count + 1);
+                let copy = [...count];
+                copy[i]++;
+                setCount(copy);
               }}
             >
               +
