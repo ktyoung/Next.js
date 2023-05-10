e7c9e96394b5d67418fe6417bb4c3f354c299135
ktyoung <ktyoung0507@gmail.com>
2023-05-10 21:11:07 +0900

    DB 데이터 출력(글 목록)

--- forum/app/page.js
+++ forum/app/page.js
@@ -1,14 +1,13 @@
-// 1. MongoDB 세팅
-// 1-1. npm install mongodb
-// 1-2. db 접속 → util/database.js에 세팅 코드 작성
-
-// 2. DB 입출력 코드는 server comoponent 안에서만 사용하는 것이 좋음
 import { connectDB } from "@/util/database";
 
 export default async function Home() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-  console.log(result);
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>안녕하세요</h1>
+      <h3>게시판입니다.</h3>
+    </div>
+  );
 }
