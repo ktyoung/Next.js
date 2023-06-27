a5d77ec44368a0b10078a49ea71fa95665df7faf
ktyoung <ktyoung0507@gmail.com>
2023-06-27 21:03:32 +0900

    게시판 프로젝트 생성

--- forum/app/page.js
+++ forum/app/page.js
@@ -1,8 +1,6 @@
-export default async function Home() {
-  return (
-    <div>
-      <h1>안녕하세요</h1>
-      <h3>게시판입니다.</h3>
-    </div>
-  );
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return <div>안녕하세요.</div>;
 }
